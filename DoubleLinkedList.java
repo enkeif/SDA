e52b3ccedec8e293e15216a0f3e9d83cbfc5a87a
Nikolina Doycheva <enkeif@abv.bg>
2016-02-05 19:33:00 +0200

    Update DoubleLinkedList.java

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -5,5 +5,24 @@ import sda.Node;
  * @param 
  * 
  */
- 
+ public class DoubleLinkedList<T extends Comparable<T>> {
+        private Node<T> head;
+       	private Node<T> tail;
+       
+       	public void setHead(Node<T> head) {
+       		    this.head = head;
+       	}
+       
+       	public void setTail(Node<T> tail) {
+       	   	 this.tail = tail;
+       	}
+       
+       	public Node<T> getHead() {
+       		    return head;
+       	}
+       
+       	public Node<T> getTail() {
+       	    	return tail;
+       	}
+
  
