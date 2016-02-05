0ed4a886f14d1ce22c1f352558f7b640e4e46671
Nikolina Doycheva <enkeif@abv.bg>
2016-02-05 19:16:39 +0200

    Update Node.java

--- Node.java
+++ Node.java
@@ -16,3 +16,20 @@ public class Node<T> {
 	public void setData(T data) {
 		this.data = data;
 	}
+	public Node<T> getLeft() {
+		return left;
+	}
+
+	public void setLeft(Node<T> left) {
+		this.left = left;
+	}
+
+	public Node<T> getRight() {
+		return right;
+	}
+
+	public void setRight(Node<T> right) {
+		this.right = right;
+	}
+	
+
